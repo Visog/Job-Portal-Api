a297eeab61c9ee736e5327abc9e3643fdd60dbaa
MUBARAK\sak <Mubharak@visog.com>
2016-12-29 10:24:41 +0530

    mubarak coommmm

--- visog-job-portal-api/src/main/java/com/visog/jobportal/serviceimpl/transaction/PostJobserviceImpl.java
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/serviceimpl/transaction/PostJobserviceImpl.java
@@ -1,5 +1,6 @@
 package com.visog.jobportal.serviceimpl.transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -14,6 +15,7 @@ import com.visog.jobportal.model.transaction.Users;
 import com.visog.jobportal.req.transaction.PostJobReq;
 import com.visog.jobportal.res.transaction.PostJobRes;
 import com.visog.jobportal.service.transaction.PostJobService;
+import com.visog.jobportal.utils.DaoUtils;
 
 public class PostJobserviceImpl implements PostJobService {
 
@@ -42,16 +44,49 @@ public class PostJobserviceImpl implements PostJobService {
 		postJob.setMinExperience(req.getMinExperience());
 		postJob.setPhoneNo(req.getPhoneNo());
 		postJob.setWalkIn(req.getWalkIn());
+		
+		DaoUtils.setEntityCreateAuditColumns(postJob);
+		
+		dao.save(postJob);
+		
+		logger.info(" postjob Successfully"+postJob.getId());
 
 	}
 
 	public void updatePostJob(PostJobReq req, String postJobId) {
+		
+		PostJob postJob =(PostJob) dao.getByKey(PostJob.class, postJobId);
+		Currency currency = new Currency();
+		Status status = new Status();
+		Users users = new Users();
+		
+		postJob.setJobTitle(req.getJobTitle());
+		postJob.setJobDescription(req.getJobDescription());
+		postJob.setAnnualPackageFrom(req.getAnnualPackageFrom());
+		postJob.setAnnualPackageTo(req.getAnnualPackageTo());
+		currency.setId(req.getCurrency());
+		postJob.setCompany(req.getCompany());
+		postJob.setEffectedDateFrom(req.getEffectedDateFrom());
+		postJob.setEffectedDateTo(req.getEffectedDateTo());
+		status.setId(req.getStatus());
+		postJob.setMinExperience(req.getMinExperience());
+		postJob.setPhoneNo(req.getPhoneNo());
+		postJob.setWalkIn(req.getWalkIn());
+		
+		
+		dao.update(postJob);
+		logger.info("PostJob updated Successfully"+postJob.getId());
 		
 		
 
 	}
 
 	public List<PostJobRes> getpostjobs() {
+		
+		List<PostJob> postJobs =dao.getPostJobs();
+		
+		List<PostJobRes> postJobList =new ArrayList<>();
+		
 		
 		return null;
 	}
