ca46535b1c3e670eff70ee4add9c16c707c796ca
user <user@user-PC>
2017-01-02 11:13:59 +0530

    commit by ravi

---
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/service/transaction/FilesService.java
@@ -0,0 +1,21 @@
+package com.visog.jobportal.service.transaction;
+
+import java.util.List;
+
+import com.visog.jobportal.model.transaction.Files;
+import com.visog.jobportal.req.transaction.FilesReq;
+import com.visog.jobportal.res.transaction.FilesRes;
+
+public interface FilesService {
+	
+	public void saveFiles(FilesReq req);
+	
+	public void updateFiles(FilesReq req, String filesId);
+	
+	public List<FilesRes> getFiles(String Id);
+	
+	public Boolean deleteFiles(String filesId);
+	
+	
+
+}
