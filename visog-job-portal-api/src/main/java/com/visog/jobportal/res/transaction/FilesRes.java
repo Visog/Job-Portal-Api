f8075c234f077214775d9765913a7f7abb1b0872
user <user@user-PC>
2017-01-02 10:49:20 +0530

    commit by ravi

---
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/res/transaction/FilesRes.java
@@ -0,0 +1,56 @@
+package com.visog.jobportal.res.transaction;
+
+public class FilesRes {
+	
+	private String id;
+
+	private String filepath;
+	
+	private String associatedtype;
+	
+	private String associated;
+	
+	private String filetype;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+
+	public String getAssociatedtype() {
+		return associatedtype;
+	}
+
+	public void setAssociatedtype(String associatedtype) {
+		this.associatedtype = associatedtype;
+	}
+
+	public String getAssociated() {
+		return associated;
+	}
+
+	public void setAssociated(String associated) {
+		this.associated = associated;
+	}
+
+	public String getFiletype() {
+		return filetype;
+	}
+
+	public void setFiletype(String filetype) {
+		this.filetype = filetype;
+	}
+	
+	
+}
