1e2efeb7b8534157d2cc4b31db0863f0feb9417a
MUBARAK\sak <Mubharak@visog.com>
2017-01-03 11:35:10 +0530

    committed

---
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/res/transaction/PostJobSeekerRes.java
@@ -0,0 +1,57 @@
+package com.visog.jobportal.res.transaction;
+
+public class PostJobSeekerRes {
+
+	private String id;
+
+	private String jobSeeker;
+
+	private String employer;
+
+	private String postJob;
+
+	private String status;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getJobSeeker() {
+		return jobSeeker;
+	}
+
+	public void setJobSeeker(String jobSeeker) {
+		this.jobSeeker = jobSeeker;
+	}
+
+	public String getEmployer() {
+		return employer;
+	}
+
+	public void setEmployer(String employer) {
+		this.employer = employer;
+	}
+
+	public String getPostJob() {
+		return postJob;
+	}
+
+	public void setPostJob(String postJob) {
+		this.postJob = postJob;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+
+}
