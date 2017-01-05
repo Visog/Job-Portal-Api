26465158fecfb10381d21ffed47f38f07f5ca0d3
user <user@user-PC>
2017-01-05 10:14:07 +0530

    ravi

--- visog-job-portal-api/src/main/java/com/visog/jobportal/serviceimpl/transaction/LanguageKnownServiceImpl.java
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/serviceimpl/transaction/LanguageKnownServiceImpl.java
@@ -53,22 +53,22 @@ public class LanguageKnownServiceImpl implements LanguageKnownService{
 	 */
 	public void updateLanguageKnown(LanguageKnownReq req, String languageKnownId) {
 
-		LanguageKnown languageKnown=(LanguageKnown) dao.getByKey(LanguageKnown.class, languageKnownId);
-		languageKnown.setRead(req.getRead());
-		languageKnown.setWrite(req.getWrite());
-		languageKnown.setSpeak(req.getSpeak());
-		languageKnown.setProficencylevel(req.getProficencylevel());
+		LanguageKnown languageknown=(LanguageKnown) dao.getByKey(LanguageKnown.class, languageKnownId);
+		languageknown.setRead(req.getRead());
+		languageknown.setWrite(req.getWrite());
+		languageknown.setSpeak(req.getSpeak());
+		languageknown.setProficencylevel(req.getProficencylevel());
 		
 		Languages languages=new Languages();
 		languages.setId(req.getLanguage());
-		languageKnown.setLanguage(languages);
+		languageknown.setLanguage(languages);
 		
 		Users users=new Users();
 		users.setId(req.getUser());
-		languageKnown.setUser(users);
+		languageknown.setUser(users);
 		
-		dao.update(languageKnown);
-		logger.info(" languageKnown updated Successfully"+ languageKnown.getId());
+		dao.update(languageknown);
+		logger.info(" languageKnown updated Successfully"+ languageknown.getId());
 		
 	}
 
@@ -78,21 +78,21 @@ public class LanguageKnownServiceImpl implements LanguageKnownService{
 	 */
 	public List<LanguageKnownRes> getLanguageKnown() {
 		
-		List<LanguageKnown> languageKnowns=dao.getLanguagesKnown();
+		List<LanguageKnown> languageknowns=dao.getLanguagesKnown();
 		List<LanguageKnownRes> languageKnownList=new ArrayList<>();
 		LanguageKnownRes languageKnownRes=null;
 		Languages languages=new Languages();
 		
-		for(LanguageKnown languageKnown:languageKnowns){
+		for(LanguageKnown languageknown:languageknowns){
 			
 		languageKnownRes =new LanguageKnownRes();
-		languageKnownRes.setId(languageKnown.getId());
-		languageKnownRes.setRead(languageKnown.getRead());
-		languageKnownRes.setSpeak(languageKnown.getSpeak());
-		languageKnownRes.setProficencylevel(languageKnown.getProficencylevel());
-		languageKnownRes.setWrite(languageKnown.getWrite());
-		languageKnownRes.setLanguage(languageKnown.getLanguage().getId());
-		languageKnownRes.setUser(languageKnown.getUser().getId());
+		languageKnownRes.setId(languageknown.getId());
+		languageKnownRes.setRead(languageknown.getRead());
+		languageKnownRes.setSpeak(languageknown.getSpeak());
+		languageKnownRes.setProficencylevel(languageknown.getProficencylevel());
+		languageKnownRes.setWrite(languageknown.getWrite());
+		languageKnownRes.setLanguage(languageknown.getLanguage().getId());
+		languageKnownRes.setUser(languageknown.getUser().getId());
 		
 		languageKnownList.add(languageKnownRes);
 			
