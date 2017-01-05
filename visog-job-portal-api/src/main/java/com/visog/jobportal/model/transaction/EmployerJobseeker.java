d11e34baf106628e7730e94632a93629e974b3f4
user <user@user-PC>
2017-01-05 16:04:52 +0530

    ravi

--- visog-job-portal-api/src/main/java/com/visog/jobportal/model/transaction/EmployerJobseeker.java
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/model/transaction/EmployerJobseeker.java
@@ -1,5 +1,6 @@
 package com.visog.jobportal.model.transaction;
 
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,31 +8,53 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.visog.jobportal.model.AbstractModel;
 
 @Table(name = "EMPLOYER_JOBSEEKER")
 @Entity
-public class EmployerJobseeker extends AbstractModel{
-	
+public class EmployerJobseeker extends AbstractModel {
+
 	@Id
 	@Column(name = "ID")
 	private String id;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "EMPLOYER_ID")
 	private Users employer;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "JOB_SEEKER_ID")
 	private Users jobseeker;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "STATUS_ID")
 	private Users status;
-	
+
 	@Column(name = "ACTION_DONE_BY")
 	private String actiondone;
+
+	@Column(name = "CREATED_SID")
+	private String createdSid;
+
+	@Column(name = "CREATED_BY")
+	private String createdBy;
+
+	@Column(name = "CREATED_ON")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdOn;
+
+	@Column(name = "UPDATED_SID")
+	private String updatedSid;
+
+	@Column(name = "UPDATED_BY")
+	private String updatedBy;
+
+	@Column(name = "UPDATED_ON")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updatedOn;
 
 	public String getId() {
 		return id;
@@ -73,7 +96,52 @@ public class EmployerJobseeker extends AbstractModel{
 		this.actiondone = actiondone;
 	}
 
-	
+	public String getCreatedSid() {
+		return createdSid;
+	}
 
-	
+	public void setCreatedSid(String createdSid) {
+		this.createdSid = createdSid;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public String getUpdatedSid() {
+		return updatedSid;
+	}
+
+	public void setUpdatedSid(String updatedSid) {
+		this.updatedSid = updatedSid;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Date getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(Date updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+
 }
--- visog-job-portal-api/src/main/java/com/visog/jobportal/model/transaction/LanguageKnown.java
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/model/transaction/LanguageKnown.java
@@ -1,42 +1,66 @@
 package com.visog.jobportal.model.transaction;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.visog.jobportal.model.AbstractModel;
 import com.visog.jobportal.model.master.Languages;
 
 @Table(name = "LANGUAGE_KNOWN")
 @Entity
-public class LanguageKnown  extends AbstractModel{
-	
+public class LanguageKnown extends AbstractModel {
+
 	@Id
 	@Column(name = "ID")
 	private String id;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "USER_ID")
 	private Users user;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "LANGUAGE_ID")
 	private Languages language;
-	
+
 	@Column(name = "READ_ID")
 	private String read;
-	
+
 	@Column(name = "WRITE_ID")
 	private String write;
-	
+
 	@Column(name = "SPEAK")
 	private String speak;
-	
+
 	@Column(name = "PROFICENCY_LEVEL")
 	private String proficencylevel;
+
+	@Column(name = "CREATED_SID")
+	private String createdSid;
+
+	@Column(name = "CREATED_BY")
+	private String createdBy;
+
+	@Column(name = "CREATED_ON")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdOn;
+
+	@Column(name = "UPDATED_SID")
+	private String updatedSid;
+
+	@Column(name = "UPDATED_BY")
+	private String updatedBy;
+
+	@Column(name = "UPDATED_ON")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updatedOn;
 
 	public String getId() {
 		return id;
@@ -93,6 +117,53 @@ public class LanguageKnown  extends AbstractModel{
 	public void setProficencylevel(String proficencylevel) {
 		this.proficencylevel = proficencylevel;
 	}
-	
+
+	public String getCreatedSid() {
+		return createdSid;
+	}
+
+	public void setCreatedSid(String createdSid) {
+		this.createdSid = createdSid;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public String getUpdatedSid() {
+		return updatedSid;
+	}
+
+	public void setUpdatedSid(String updatedSid) {
+		this.updatedSid = updatedSid;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Date getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(Date updatedOn) {
+		this.updatedOn = updatedOn;
+	}
 
 }
