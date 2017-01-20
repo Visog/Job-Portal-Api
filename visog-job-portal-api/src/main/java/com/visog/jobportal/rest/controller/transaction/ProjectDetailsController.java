14dd31973d4fb237151b60f4b445c8374f88aee7
sys <sys@shivaprasad>
2017-01-20 13:35:43 +0530

    By Mubarak

--- visog-job-portal-api/src/main/java/com/visog/jobportal/rest/controller/transaction/ProjectDetailsController.java
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/rest/controller/transaction/ProjectDetailsController.java
@@ -15,20 +15,32 @@ import javax.ws.rs.core.MediaType;
 import org.apache.log4j.Logger;
 
 import com.visog.jobportal.constants.Status;
-import com.visog.jobportal.req.transaction.PostJobDomainsReq;
+
 import com.visog.jobportal.req.transaction.ProjectDetailsReq;
+import com.visog.jobportal.req.transaction.UsersReq;
 import com.visog.jobportal.res.master.JobPortalResponse;
+import com.visog.jobportal.rest.controller.transaction.ProjectDetailsController;
 import com.visog.jobportal.service.transaction.ProjectDetailsService;
+
+import com.visog.jobportal.service.transaction.UserService;
 
 @Path("/transaction")
 @Produces(MediaType.APPLICATION_JSON)
 public class ProjectDetailsController {
 	private static final Logger logger = Logger.getLogger(ProjectDetailsController.class);
-	
+
 	private @CookieParam("User-Identifier") String userIdentifier;
 
 	@Inject
 	private ProjectDetailsService service;
+
+	/**
+	 * This method creates the Projectdetails
+	 * 
+	 * @param req
+	 * @return
+	 */
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/projectDetails")
@@ -37,13 +49,20 @@ public class ProjectDetailsController {
 		service.saveProjectDetails(req);
 
 		JobPortalResponse jobPortalResponse = new JobPortalResponse();
-		jobPortalResponse.setMessage("Project Details saved succcessfully");
+		jobPortalResponse.setMessage("ProjectDetails saved succcessfully");
 		jobPortalResponse.setStatus(Status.STATUS_SUCCESS);
 		jobPortalResponse.setStatusCode(Status.STATUSCODE_SUCCESS);
 
 		return jobPortalResponse;
+
 	}
 
+	/**
+	 * This method updates the Role
+	 * 
+	 * @param req
+	 * @return
+	 */
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/projectDetails/{projectDetailsId}")
@@ -53,7 +72,7 @@ public class ProjectDetailsController {
 		service.updateProjectDetails(req, projectDetailsId);
 
 		JobPortalResponse jobPortalResponse = new JobPortalResponse();
-		jobPortalResponse.setMessage("project Details updated succcessfully");
+		jobPortalResponse.setMessage("ProjectDetails updated succcessfully");
 		jobPortalResponse.setStatus(Status.STATUS_SUCCESS);
 		jobPortalResponse.setStatusCode(Status.STATUSCODE_SUCCESS);
 
@@ -61,20 +80,31 @@ public class ProjectDetailsController {
 
 	}
 
+	/**
+	 * This method retrieves all roles
+	 * 
+	 * @return
+	 */
 	@GET
 	@Path("/projectDetails")
 	public JobPortalResponse getProjectDetails() {
 
 		JobPortalResponse jobPortalResponse = new JobPortalResponse();
 		jobPortalResponse.setData(service.getProjectDetails());
-		jobPortalResponse.setMessage("projectDetails fetched succcessfully");
+		jobPortalResponse.setMessage("ProjectDetails fetched succcessfully");
 		jobPortalResponse.setStatus(Status.STATUS_SUCCESS);
 		jobPortalResponse.setStatusCode(Status.STATUSCODE_SUCCESS);
 
 		return jobPortalResponse;
 
-	
 	}
+
+	/**
+	 * This method retrieves a single Project
+	 * 
+	 * @return
+	 */
+
 	@GET
 	@Path("/projectDetails/{projectDetailsId}")
 	public JobPortalResponse getProjectDetails(@PathParam("projectDetailsId") String projectDetailsId) {
@@ -88,6 +118,13 @@ public class ProjectDetailsController {
 		return jobPortalResponse;
 
 	}
+
+	/**
+	 * This method delete the role
+	 * 
+	 * @return
+	 */
+
 	@DELETE
 	@Path("/projectDetails/{projectDetailsId}")
 	public JobPortalResponse deleteProjectDetails(@PathParam("projectDetailsId") String projectDetailsId) {
@@ -95,16 +132,16 @@ public class ProjectDetailsController {
 		JobPortalResponse jobPortalResponse = new JobPortalResponse();
 
 		if (service.deleteProjectDetails(projectDetailsId)) {
-			jobPortalResponse.setMessage("projectDetails deleted succcessfully");
+			jobPortalResponse.setMessage("ProjectDetails deleted succcessfully");
 			jobPortalResponse.setStatus(Status.STATUS_SUCCESS);
 			jobPortalResponse.setStatusCode(Status.STATUSCODE_SUCCESS);
 		} else {
-			jobPortalResponse.setMessage("Failed to delete the projectDetails");
+			jobPortalResponse.setMessage("Failed to delete the ProjectDetails");
 			jobPortalResponse.setStatus(Status.STATUS_FAIL);
 			jobPortalResponse.setStatusCode(Status.STATUSCODE_FAIL);
 		}
 
 		return jobPortalResponse;
-	}
 
+	}
 }
--- visog-job-portal-api/src/main/java/com/visog/jobportal/service/transaction/ProjectDetailsService.java
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/service/transaction/ProjectDetailsService.java
@@ -1,20 +1,21 @@
 package com.visog.jobportal.service.transaction;
 
 import java.util.List;
-
 import com.visog.jobportal.req.transaction.ProjectDetailsReq;
 import com.visog.jobportal.res.transaction.ProjectDetailsRes;
 
 public interface ProjectDetailsService {
-	
+
 	public void saveProjectDetails(ProjectDetailsReq req);
 
 	public void updateProjectDetails(ProjectDetailsReq req, String projectDetailsId);
 
 	public List<ProjectDetailsRes> getProjectDetails();
-
+	
 	public ProjectDetailsRes getProjectDetails(String id);
 
 	public Boolean deleteProjectDetails(String projectDetailsId);
+
+	
 
 }
