49cddef3f7e698b5892acc8028d706eda31a358f
sys <sys@sys-PC>
2017-02-02 17:35:43 +0530

    by mubark

--- visog-job-portal-api/src/main/java/com/visog/jobportal/constants/AppConstants.java
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/constants/AppConstants.java
@@ -9,5 +9,8 @@ public interface AppConstants {
 	String ADMIN_NAME="admin";
 	String userId="08d344ed-61c6-4b04-8638-76f5eaa60f5d";
 	
+	String SERVICE_KEY = "service_key";
+	
+	String AUTH_TOKEN = "auth_token";
 	
 }
--- visog-job-portal-api/src/main/java/com/visog/jobportal/rest/controller/common/LoginController.java
+++ visog-job-portal-api/src/main/java/com/visog/jobportal/rest/controller/common/LoginController.java
@@ -15,8 +15,10 @@ import javax.ws.rs.core.MediaType;
 import org.apache.log4j.Logger;
 import com.visog.jobportal.constants.Status;
 import com.visog.jobportal.req.common.LoginReq;
+import com.visog.jobportal.req.jobseeker.JobSeekerReq;
 import com.visog.jobportal.res.master.JobPortalResponse;
 import com.visog.jobportal.service.common.LoginService;
+import com.visog.jobportal.servlet.RestInterceptor;
 
 @Path("/Login")
 @Produces(MediaType.APPLICATION_JSON)
@@ -25,40 +27,55 @@ public class LoginController {
 	private static final Logger logger = Logger.getLogger(LoginController.class);
 
 	private @CookieParam("User-Identifier") String userIdentifier;
-	
+
 	@Inject
 	private LoginService service;
+
 	
-	
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/details")
-	public JobPortalResponse createState(LoginReq req) {
+	public JobPortalResponse login(LoginReq req) {
 
 		JobPortalResponse jobPortalResponse = new JobPortalResponse();
-		if(service.isUsersLoginExists(req.getPassword(), req.getEmail())){
-	       jobPortalResponse.setData(service.getDetails(req.getEmail()));		
+		if (service.isUsersLoginExists(req.getPassword(), req.getEmail())) {
+			jobPortalResponse.setData(service.getDetails(req.getEmail()));
 			jobPortalResponse.setMessage("Login succcessfully");
 			jobPortalResponse.setStatus(Status.STATUS_SUCCESS);
 			jobPortalResponse.setStatusCode(Status.STATUSCODE_SUCCESS);
-			}else{
-				jobPortalResponse.setMessage("Failed to login!!!");
-				jobPortalResponse.setStatus(Status.STATUS_FAIL);
-				jobPortalResponse.setStatusCode(Status.STATUSCODE_FAIL);
-			}
-
+		} else {
+			jobPortalResponse.setMessage("Failed to login!!!");
+			jobPortalResponse.setStatus(Status.STATUS_FAIL);
+			jobPortalResponse.setStatusCode(Status.STATUSCODE_FAIL);
+		}
 
 		return jobPortalResponse;
 
 	}
 
-	
+	/*@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/restInterceptors")
+	public JobPortalResponse loginInterceptor(LoginReq req) {
+		return null;
 
-	
+		
+		 * JobPortalResponse jobPortalResponse = new JobPortalResponse();
+		 * if(service.isUsersLoginExists(req.getPassword(), req.getEmail())){
+		 * jobPortalResponse.setData(service.getDetails(req.getEmail()));
+		 * jobPortalResponse.setMessage("Login succcessfully");
+		 * jobPortalResponse.setStatus(Status.STATUS_SUCCESS);
+		 * jobPortalResponse.setStatusCode(Status.STATUSCODE_SUCCESS); }else{
+		 * jobPortalResponse.setMessage("Failed to login!!!");
+		 * jobPortalResponse.setStatus(Status.STATUS_FAIL);
+		 * jobPortalResponse.setStatusCode(Status.STATUSCODE_FAIL); }
+		 * 
+		 * 
+		 * return jobPortalResponse;
+		 
 
-	
-
-
-
+		
+	}
+*/
 }
